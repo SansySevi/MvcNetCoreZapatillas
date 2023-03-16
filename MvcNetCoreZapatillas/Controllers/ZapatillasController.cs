340bdf3fb3cb0e4a190ad087a3c2c7a8c6e1661e
SansySevi <ssevilleja@hotmail.es>
2023-03-16 14:33:42 +0100

    Add project files.

---
+++ MvcNetCoreZapatillas/Controllers/ZapatillasController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreZapatillas.Models;
+using MvcNetCoreZapatillas.Repositories;
+
+namespace MvcNetCoreZapatillas.Controllers
+{
+    public class ZapatillasController : Controller
+    {
+        private RepositoryZapatillas repo;
+
+        public ZapatillasController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Home()
+        {
+            List<Zapatilla> zapatillas = await this.repo.GetZapatillas();
+            return View(zapatillas);
+        }
+
+        public async Task<IActionResult> Details(int idproducto)
+        {
+            Zapatilla zapatilla = await this.repo.GetZapatilla(idproducto);
+            return View(zapatilla);
+        }
+
+        public async Task<IActionResult> _PaginacionZapatillas(int? posicion, int idproducto)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+
+            List<ImagenZapatilla> imagenes =
+                    await this.repo.GetImagenesZapatillas(posicion.Value, idproducto);
+            ViewData["REGISTROS"] = this.repo.GetNumeroZapartillas(idproducto);
+            ViewData["IDPRODUCTO"] = idproducto;
+            return PartialView("_PaginacionZapatillas", imagenes);
+        }
+
+
+        public async Task<IActionResult> _DetalleImagen(int? posicion, int idproducto)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+
+            List<ImagenZapatilla> imagenes =
+                    await this.repo.GetImagenesZapatillas(posicion.Value, idproducto);
+            ViewData["REGISTROS"] = this.repo.GetNumeroZapartillas(idproducto);
+            ViewData["IDPRODUCTO"] = idproducto;
+            return PartialView("_PaginacionZapatillas", imagenes);
+        }
+    }
+}
---
+++ MvcNetCoreZapatillas/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MvcNetCoreZapatillas.Data;
+using MvcNetCoreZapatillas.Models;
+
+#region SQL SERVER
+//VUESTRO PROCEDIMIENTO DE PAGINACION DE IMAGENES DE ZAPATILLAS
+#endregion
+
+#region
+
+//alter PROCEDURE SP_IMAGENES_ZAPATILLAS
+//(@POSICION INT, @IDPRODUCTO INT)
+//AS
+//    SELECT IDIMAGEN, IDPRODUCTO, IMAGEN FROM
+//        (SELECT CAST(
+//            ROW_NUMBER() OVER(ORDER BY IDPRODUCTO) AS INT) AS POSICION,
+//            IDIMAGEN, IDPRODUCTO, IMAGEN
+//        FROM IMAGENESZAPASPRACTICA
+//        WHERE IDPRODUCTO = @IDPRODUCTO) AS QUERY
+//    WHERE QUERY.POSICION >= @POSICION AND QUERY.POSICION < (@POSICION + 1)
+//GO
+
+#endregion
+
+namespace MvcNetCoreZapatillas.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Zapatilla>> GetZapatillas()
+        {
+            return await this.context.Zapatillas.ToListAsync();
+        }
+
+        public async Task<Zapatilla> GetZapatilla(int idproducto)
+        {
+            Zapatilla zapatilla = await this.context.Zapatillas.
+                FirstOrDefaultAsync(z => z.IdProducto == idproducto);
+            return zapatilla;
+        }
+
+        public int GetNumeroZapartillas(int idproducto)
+        {
+            return this.context.Zapatillas.
+                Where(z => z.IdProducto == idproducto).Count();
+        }
+
+
+        /*REVISAR*/
+        public async Task<List<ImagenZapatilla>>
+        GetImagenesZapatillas(int posicion, int idproducto)
+        {
+            string sql =
+                "SP_IMAGENES_ZAPATILLAS @POSICION, @IDPRODUCTO";
+            SqlParameter pamposicion =
+                new SqlParameter("@POSICION", posicion);
+            SqlParameter pamidproducto =
+                new SqlParameter("@IDPRODUCTO", idproducto);
+
+            var consulta =
+                this.context.ImagenesZapatillas.FromSqlRaw(sql, pamposicion, pamidproducto);
+            List<ImagenZapatilla> imagenes = await consulta.ToListAsync();
+
+            return imagenes;
+        }
+
+        //public async Task<List<ImagenZapatilla>>
+        //getImagen(int posicion, int idproducto)
+        //{
+        //    string sql =
+        //        "SP_IMAGEN_ZAPATILLAS @POSICION, @IDPRODUCTO";
+        //    SqlParameter pamposicion =
+        //        new SqlParameter("@POSICION", posicion);
+        //    SqlParameter pamidproducto =
+        //        new SqlParameter("@IDPRODUCTO", idproducto);
+
+        //    var consulta =
+        //        this.context.ImagenesZapatillas.FromSqlRaw(sql, pamposicion, pamidproducto);
+        //    List<ImagenZapatilla> imagenes = await consulta.ToListAsync();
+
+        //    return imagenes;
+        //}
+
+
+    }
+}
